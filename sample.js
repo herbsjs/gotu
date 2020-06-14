a838c43e49e8068456a096f6a6c708ec04de0c60
Jhonathan S Soares <jhonathanmarolo@hotmail.com>
2020-06-13 23:00:22 -0300

    format file sample

--- sample.js
+++ sample.js
@@ -1,31 +1,29 @@
 var user = entity('User', {
+  username: field(String, {
+    presence: true,
+    exclusion: {
+      within: ['nicklas'],
+      message: "'%{value}' is not allowed",
+    },
+  }),
 
-    username: field(String, {
-        presence: true,
-        exclusion: {
-            within: ["nicklas"],
-            message: "'%{value}' is not allowed"
-        }
-    }),
+  password: field(String, {
+    presence: true,
+    length: {
+      minimum: 6,
+      message: 'must be at least 6 characters',
+    },
+  }),
 
-    password: field(String, {
-        presence: true,
-        length: {
-            minimum: 6,
-            message: "must be at least 6 characters"
-        }
-    }),
+  email: field(String, {
+    presence: true,
+    length: {
+      minimum: 6,
+      message: 'must be at least 6 characters',
+    },
+  }),
 
-    email: field(String, {
-        presence: true,
-        length: {
-            minimum: 6,
-            message: "must be at least 6 characters"
-        }
-    }),
-
-    lastAccess: field(Date)
-
+  lastAccess: field(Date),
 })
 
-validate({ password: "bad" }, constraints)
+validate({ password: 'bad' }, constraints)
