c96b019ed3d442979333038024d48234a15574ed
Enderson Costa <enderson.tech@gmail.com>
2020-08-10 13:45:55 -0300

    fix lint

--- src/baseEntity.js
+++ src/baseEntity.js
@@ -1,98 +1,98 @@
-const { validate, checker } = require("suma");
-const validateValue = validate;
+const { validate, checker } = require("suma")
+const validateValue = validate
 
 class BaseEntity {
   validate() {
-    const errors = {};
+    const errors = {}
     for (const [name, definition] of Object.entries(this.meta.schema)) {
-      const value = this[name];
+      const value = this[name]
 
       // ignore functions
-      if (checker.isFunction(value)) continue;
+      if (checker.isFunction(value)) continue
 
       // types validation
-      const validation = definition.validation;
-      const retErrors = validateValue(value, validation);
+      const validation = definition.validation
+      const retErrors = validateValue(value, validation)
       if (retErrors.errors && retErrors.errors.length > 0) {
-        errors[name] = retErrors.errors;
-        continue;
+        errors[name] = retErrors.errors
+        continue
       }
 
       // for entity types (deep validation)
       if (value instanceof BaseEntity) {
-        if (value.isValid()) continue;
-        errors[name] = value.errors;
+        if (value.isValid()) continue
+        errors[name] = value.errors
       }
 
       // for array of entity types
       if (Array.isArray(value) && value.baseType.prototype instanceof BaseEntity) {
         const errorList = value.map((item) =>
           !item.isValid() ? item.errors : null
-        );
-        const errorFound = errorList.filter((error) => !!error);
+        )
+        const errorFound = errorList.filter((error) => !!error)
 
-        if (!errorFound || !errorFound.length) continue;
-        errors[name] = errorList;
+        if (!errorFound || !errorFound.length) continue
+        errors[name] = errorList
       }
     }
 
-    this.__proto__.errors = errors;
+    this.__proto__.errors = errors
   }
 
   isValid() {
-    this.validate();
-    return Object.keys(this.errors).length === 0;
+    this.validate()
+    return Object.keys(this.errors).length === 0
   }
 
   toJSON() {
     function deepCopy(obj) {
-      const copy = {};
+      const copy = {}
       for (const field in obj.meta.schema) {
-        let value = obj[field];
-        if (value instanceof BaseEntity) value = deepCopy(value);
-        if (value instanceof Function) continue;
-        copy[field] = value;
+        let value = obj[field]
+        if (value instanceof BaseEntity) value = deepCopy(value)
+        if (value instanceof Function) continue
+        copy[field] = value
       }
-      return copy;
+      return copy
     }
-    const obj = deepCopy(this);
-    return obj;
+    const obj = deepCopy(this)
+    return obj
   }
 
   static fromJSON(json) {
     function parse(type, value) {
-      if (value === undefined) return undefined;
-      if (value === null) return null;
+      if (value === undefined) return undefined
+      if (value === null) return null
 
-      if (type === Date) return new Date(value);
+      if (type === Date) return new Date(value)
       if (type.prototype instanceof BaseEntity) {
-        const entity = type.fromJSON(value);
-        return entity;
+        const entity = type.fromJSON(value)
+        return entity
       }
 
       if (Array.isArray(type) && type.baseType.prototype instanceof BaseEntity) {
         return value.map((item) => {
-          const entity = type.baseType.fromJSON(item);
-          return entity;
-        });
+          const entity = type.baseType.fromJSON(item)
+          return entity
+        })
       }
 
-      return value;
+      return value
     }
 
-    let data = json;
-    if (typeof json === "string") data = JSON.parse(json);
+    let data = json
+    if (typeof json === "string") data = JSON.parse(json)
 
-    const instance = new this();
+    const instance = new this()
 
     for (const field in instance.meta.schema) {
-      const fieldMeta = instance.meta.schema[field];
-      if (!(fieldMeta.constructor.name === "Field")) continue;
-      instance[field] = parse(fieldMeta.type, data[field]);
+      const fieldMeta = instance.meta.schema[field]
+      if (!(fieldMeta.constructor.name === "Field")) continue
+      instance[field] = parse(fieldMeta.type, data[field])
     }
 
-    return instance;
+    return instance
   }
 }
 
-module.exports = { BaseEntity };
+module.exports = { BaseEntity }
--- src/field.js
+++ src/field.js
@@ -1,49 +1,49 @@
-const { BaseEntity } = require("./baseEntity");
+const { BaseEntity } = require("./baseEntity")
 
 class Field {
   constructor(type, options = {}) {
-    this.name = "";
-    this.type = type;
-    this.options = options;
-    this._validations = null;
+    this.name = ""
+    this.type = type
+    this.options = options
+    this._validations = null
   }
 
   get defaultValue() {
     if (this.options.default !== undefined) {
       if (typeof this.options.default === "function")
-        return this.options.default();
-      return this.options.default;
+        return this.options.default()
+      return this.options.default
     }
 
-    const type = this.type;
-    if (type === Number) return 0;
-    if (type === String) return "";
-    if (type === Date) return null;
-    if (type === Boolean) return false;
-    if (type.prototype instanceof BaseEntity) return new type();
+    const type = this.type
+    if (type === Number) return 0
+    if (type === String) return ""
+    if (type === Date) return null
+    if (type === Boolean) return false
+    if (type.prototype instanceof BaseEntity) return new type()
 
     if (Array.isArray(type) && type[0].prototype instanceof BaseEntity) {
-      const arr = new Array();
-      arr.__proto__.baseType = type[0];
-      return arr;
+      const arr = new Array()
+      arr.__proto__.baseType = type[0]
+      return arr
     }
 
-    return undefined;
+    return undefined
   }
 
   get validation() {
-    if (this._validations) return this._validations;
+    if (this._validations) return this._validations
 
-    const validation = { type: this.type };
+    const validation = { type: this.type }
     if (this.options.validation)
-      Object.assign(validation, this.options.validation);
+      Object.assign(validation, this.options.validation)
 
-    return (this._validations = validation);
+    return (this._validations = validation)
   }
 }
 
 const field = (type, options) => {
-  return new Field(type, options);
-};
+  return new Field(type, options)
+}
 
-module.exports = { field, Field };
+module.exports = { field, Field }
